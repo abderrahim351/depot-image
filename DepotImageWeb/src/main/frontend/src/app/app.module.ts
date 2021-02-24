5b479c6468fbfb38f77ac8d6a18dbb6db1ffc668
aymen <aymen@p50.home>
2021-02-24 22:00:17 +0100

    primeNg

--- DepotImageWeb/src/main/frontend/src/app/app.module.ts
+++ DepotImageWeb/src/main/frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
