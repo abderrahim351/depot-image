d3c0c62e9024f3ea70047d5f3eb3b804fe1726d3
Aymen JAMAAOUI <cgi_ayj@p50>
2021-06-08 01:09:20 +0200

    metadatas

--- DepotImageWeb/src/main/frontend/src/app/image-metas/image-metas.component.ts
+++ DepotImageWeb/src/main/frontend/src/app/image-metas/image-metas.component.ts
@@ -1,16 +1,33 @@
+import { Input } from '@angular/core';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { ImageMetasService } from './image-metas.service';
 
 @Component({
   selector: 'app-image-metas',
   templateUrl: './image-metas.component.html',
   styleUrls: ['./image-metas.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [ImageMetasService],
 })
 export class ImageMetasComponent implements OnInit {
+  private _idImg: number;
 
-  constructor() { }
+  metasTree: TreeNode[];
 
-  ngOnInit(): void {
+  constructor(private service: ImageMetasService) {}
+
+  ngOnInit(): void {}
+
+  public get idImg(): number {
+    return this._idImg;
   }
 
+  @Input()
+  public set idImg(value: number) {
+    this._idImg = value;
+    this.service.metadatas(this._idImg).then((res) => {
+      this.metasTree = res;
+    });
+  }
 }
--- DepotImageWeb/src/main/java/com/sdi/dimage/utils/MetaModel.java
+++ DepotImageWeb/src/main/java/com/sdi/dimage/utils/MetaModel.java
@@ -1,8 +1,36 @@
 package com.sdi.dimage.utils;
 
 public class MetaModel {
+	
+	private String directory;
+	
     private String tag ;
     private String valeur ;
+    
+    
+    
+	public MetaModel() {
+		super();
+	}
+	
+	public MetaModel(String directory) {
+		this.directory = directory;
+	}
+	
+	
+
+	public MetaModel(String tag, String valeur) {
+		super();
+		this.tag = tag;
+		this.valeur = valeur;
+	}
+
+	public String getDirectory() {
+		return directory;
+	}
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
 	public String getTag() {
 		return tag;
 	}
